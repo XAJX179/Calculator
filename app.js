b7d5ff13816a22dab4ea8d402ced6b1a88b255e8
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-08-05 16:07:53 +0530

    Feat : Operate Function

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function mul(a, b) {
+  return a * b;
+}
+function div(a, b) {
+  return a / b;
+}
+let firstNumber;
+let secondNumber;
+let operator;
 
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "-":
+      sub(firstNumber, secondNumber);
+      break;
+    case "*":
+      mul(firstNumber, secondNumber);
+      break;
+    case "/":
+      div(firstNumber, secondNumber);
+      break;
+    default:
+      alert("Error in Operation");
+      break;
+  }
+}
